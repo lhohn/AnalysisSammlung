d7d54c001b0000b838a546b990d2e8dd5185c31e
lhohn2s <luis-maria.hohn@smail.inf.h-brs.de>
2022-05-02 23:53:49 +0200

    Projektdateien hinzufügen.

---
+++ Verfahren/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using Verfahren.Reihen;
+
+var time = new Stopwatch();
+time.Start();
+double res = HeronVerfahren.Heron(6, 3, 5);
+time.Stop();
+
+Console.WriteLine(res+ " in " + time.ElapsedMilliseconds.ToString() +" seconds");
---
+++ Verfahren/Reihen/HeronVerfahren.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verfahren.Reihen
+{
+    public class HeronVerfahren
+    {
+        public static double Heron(int x,int p,int a)
+        {
+            if(x == 0)
+            {
+                return 1.0;
+            }
+
+            return ((p-1.0)/p)*Heron(x-1,p,a)+a/(p*Math.Pow(Heron(x-1,p,a),p-1));
+        }
+    }
+}
